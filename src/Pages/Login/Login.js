a942dc98c0b2483cc350df72f476abc83706db19
Judlup Luns <judlup@gmail.com>
2020-12-28 09:16:39 -0500

    28-12-2020 | Login works | Judlup

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,28 @@
 import React from "react";
+import firebase from "../../Initializers/firebase";
+import { Button } from "react-bootstrap";
 
 export const Login = () => {
+  function login() {
+    let provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result);
+      });
+  }
   return (
     <>
-      <h3>Login --</h3>
+      <h2>Login</h2>
+      <Button
+        variant="light"
+        onClick={() => {
+          login();
+        }}
+      >
+        Google Login
+      </Button>
     </>
   );
 };
